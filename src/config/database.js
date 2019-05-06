15881d183b37e39190737516196fca6e76ed2415
Giuliano Bortoloni <giulianoagria@hotmail.com>
2019-05-06 20:53:29 -0300

    subindo backend todoApp

---
+++ src/config/database.js
@@ -0,0 +1,9 @@
+const mongoose = require("mongoose");
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose
+  .connect("mongodb://localhost:27017/todo", {
+    useCreateIndex: true,
+    useNewUrlParser: true
+  })
+  .then(() => console.log("Connectado ao MongoDB..."));
